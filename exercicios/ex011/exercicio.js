7a919b6eaf46de0b042bd239528b70f995876323
Gabriel Souza <gabrieldesouza003@gmail.com>
2022-07-01 13:58:11 -0300

    ex 011 e  013

---
+++ exercicios/ex011/exercicio.js
@@ -0,0 +1,10 @@
+idade = 70
+if (idade < 16){
+    console.log(`Não vota`)
+} else if (idade <18){
+    console.log(`Pode Votar`)
+    } else if( idade <67){
+        console.log('vota')
+    } else {
+        console.log('Pode Votar')
+    }
