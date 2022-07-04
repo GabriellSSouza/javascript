d78c77b86d53a7ab6d8020418dab55b54937788a
Gabriel Souza <gabrieldesouza003@gmail.com>
2022-07-04 14:07:48 -0300

    exercicios

--- exercicios/ex012/exercicio.js
+++ exercicios/ex012/exercicio.js
@@ -1,7 +1,9 @@
 agora = new Date()
 hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas`)
-if (hora <12){
+if (hora <6) {
+    console.log('Boa Madrugada')
+} else if (hora <12){
     console.log('Bom Dia')
 } else if(hora <=18){
     console.log('Boa Tarde')
