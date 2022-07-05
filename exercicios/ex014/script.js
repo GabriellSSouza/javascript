a5f709300370f9d9c51034f64825c4556478aec1
Gabriel Souza <gabrieldesouza003@gmail.com>
2022-07-05 14:07:50 -0300

    exercicios

--- exercicios/ex014/script.js
+++ exercicios/ex014/script.js
@@ -1,1 +1,15 @@
-alert('olá')
+function carregar() {
+    msg = document.getElementById('msg')
+    img = document.getElementById('Imagem')
+    data = new Date()
+    hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    
+    if (hora >=0 && hora < 12) {
+        img.src = 'midia/noite.png' 
+    } else if (hora >= 12 && hora <= 18) {
+        img.src ='midia/tarde.png'
+    } else {
+        img.src = 'midia/noite.png' 
+    }
+}
