286f0bbb50f31b062632f7b4ab29f0a7615c828d
Gabriel Souza <gabrieldesouza003@gmail.com>
2022-07-07 15:33:50 -0300

    ex 15 e 16

--- exercicios/ex015/script.js
+++ exercicios/ex015/script.js
@@ -1,3 +1,49 @@
 function verificar() {
-    alert ('hahaha')
+    data = new Date()
+    ano = data.getFullYear()
+    fano = document.getElementById('txtano')
+    res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano){
+        alert('[ERRO] Verifique os dados e tente novamente')
+    }else {
+        fsex = document.getElementsByName('radsex')
+        idade = ano - Number(fano.value)
+        genero = ''
+        img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if(fsex[0].checked) {
+            genero = 'homem'
+            if(idade >=0 && idade <10){
+                //criança
+                img.setAttribute('src', 'imagens/bebe-m.png')
+            }else if (idade <21){
+                //jovem
+                img.setAttribute('src', 'imagens/jovem-m.png')
+            }else if (idade <50){
+                //adulto
+                img.setAttribute('src', 'imagens/adulto-m.png')
+            }else {
+                //idoso
+                img.setAttribute('src', 'imagens/idoso-m.png')
+            }
+        }else if (fsex[1].checked){
+            genero = 'mulher'
+            if(idade >=0 && idade <10){
+                //criança
+                img.setAttribute('src', 'imagens/bebe-f.png')
+            }else if (idade <21){
+                //jovem
+                img.setAttribute('src', 'imagens/jovem-f.png')
+            }else if (idade <50){
+                //adulto
+                img.setAttribute('src', 'imagens/adulto-f.png')
+            }else {
+                //idoso
+                img.setAttribute('src', 'imagens/idoso-f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+    }
 }
---
+++ exercicios/ex016/ambinete.js
@@ -0,0 +1,20 @@
+c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 5)
+/*
+c = 1
+while (c <= 500){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
